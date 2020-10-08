9107003ef2751cbe33e0172a23ff85f520491e68
Pawel Bozejewski <bozejewski@gmail.com>
2020-10-08 10:32:42 +0200

    firstStepOfHome

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,65 +1,185 @@
 import React, { Component } from 'react';
 import './Home.scss';
-import firstSectionImg from '../../assets/sectionFirst.jpg';
-import secondSectionImg from '../../assets/sectionSecond.jpg';
-import thirdSectionImg from '../../assets/sectionThird.jpg';
-import forthSectionImg from '../../assets/sectionForth.jpg';
+import firstSectionImgA from '../../assets/sectionFirstA.jpg';
+import firstSectionImgB from '../../assets/sectionFirstB.jpg';
+import secondSectionImgA from '../../assets/sectionSecondA.jpg';
+import secondSectionImgB from '../../assets/sectionSecondB.jpg';
+import thirdSectionImgA from '../../assets/sectionThirdA.jpg';
+import thirdSectionImgB from '../../assets/sectionThirdB.jpg';
+import forthSectionImgA from '../../assets/sectionForthA.jpg';
+import forthSectionImgB from '../../assets/sectionForthB.jpg';
 
 const itemList = [
   {
     index: 1,
-    src: firstSectionImg,
+    srcA: firstSectionImgA,
+    srcB: firstSectionImgB,
     alt: 'FirstSectionImg',
     text: 'testowy',
     letter: 'M',
     buttonitle: 'O nas',
     cName: 'letterHolder first',
-    c2: 'section firsta',
+    state: 'isHoveredFirst',
   },
   {
     index: 1,
-    src: secondSectionImg,
+    srcA: secondSectionImgA,
+    srcB: secondSectionImgB,
     alt: 'secondtSectionImg',
     text: 'testowy',
     letter: 'M',
     buttonitle: 'O nas',
     cName: 'letterHolder second',
-    c2: 'section seconda',
+    state: 'isHoveredSecond',
   },
   {
     index: 1,
-    src: thirdSectionImg,
+    srcA: thirdSectionImgA,
+    srcB: thirdSectionImgB,
     alt: 'thirdSectionImg',
     text: 'testowy',
     letter: 'M',
     buttonitle: 'O nas',
     cName: 'letterHolder third',
-    c2: 'section thirda',
+    state: 'isHoveredThird',
   },
   {
     index: 1,
-    src: forthSectionImg,
+    srcA: forthSectionImgA,
+    srcB: forthSectionImgB,
     alt: 'forthSectionImg',
     text: 'testowy',
     letter: 'M',
     buttonitle: 'O nas',
     cName: 'letterHolder forth',
-    c2: 'section fortha',
+    state: 'isHoveredForth',
   },
 ];
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHoveredFirst: false,
+      isHoveredSecond: false,
+      isHoveredThird: false,
+      isHoveredForth: false,
+    };
+  }
+
+  handleOnMouseEnterFirst = () => {
+    this.setState({ isHoveredFirst: true });
+  };
+
+  handleOnMouseLeaveFirst = () => {
+    this.setState({ isHoveredFirst: false });
+  };
+
+  handleOnMouseEnterSecond = () => {
+    this.setState({ isHoveredSecond: true });
+  };
+
+  handleOnMouseLeaveSecond = () => {
+    this.setState({ isHoveredSecond: false });
+  };
+  handleOnMouseEnterThird = () => {
+    this.setState({ isHoveredThird: true });
+  };
+
+  handleOnMouseLeaveThird = () => {
+    this.setState({ isHoveredThird: false });
+  };
+  handleOnMouseEnterForth = () => {
+    this.setState({ isHoveredForth: true });
+  };
+
+  handleOnMouseLeaveForth = () => {
+    this.setState({ isHoveredForth: false });
+  };
+
   render() {
     return (
       <div className="homeContainer" id="home">
-        {itemList.map((obj) => (
-          <div className="section">
-            <img src={obj.src} alt="firstSectionImg" />
+        {/* {itemList.map((obj) => (
+          <div
+            onMouseEnter={this.handleOnMouseEnter}
+            onMouseLeave={this.handleOnMouseLeave}
+            className="section"
+          >
+            <img
+              src={this.state.isHovered ? obj.srcA : obj.srcB}
+              alt={obj.alt}
+            />
             <div className="textSetion">
               <div className={obj.cName}></div>
             </div>
           </div>
-        ))}
+        ))} */}
+        <div
+          onMouseEnter={this.handleOnMouseEnterFirst}
+          onMouseLeave={this.handleOnMouseLeaveFirst}
+          className="section"
+        >
+          <img
+            src={
+              this.state.isHoveredFirst ? firstSectionImgA : firstSectionImgB
+            }
+            alt="test"
+          />
+          <div className="textSetion">
+            <div className="letterHolder firstA"></div>
+            <div className="letterHolder firstB"></div>
+          </div>
+        </div>
+
+        <div
+          onMouseEnter={this.handleOnMouseEnterSecond}
+          onMouseLeave={this.handleOnMouseLeaveSecond}
+          className="section"
+        >
+          <img
+            src={
+              this.state.isHoveredSecond ? secondSectionImgA : secondSectionImgB
+            }
+            alt="test"
+          />
+          <div className="textSetion">
+            <div className="letterHolder secondA"></div>
+            <div className="letterHolder secondB"></div>
+          </div>
+        </div>
+        <div
+          onMouseEnter={this.handleOnMouseEnterThird}
+          onMouseLeave={this.handleOnMouseLeaveThird}
+          className="section"
+        >
+          <img
+            src={
+              this.state.isHoveredThird ? thirdSectionImgA : thirdSectionImgB
+            }
+            alt="test"
+          />
+          <div className="textSetion">
+            <div className="letterHolder thirdA"></div>
+            <div className="letterHolder thirdB"></div>
+          </div>
+        </div>
+        <div
+          onMouseEnter={this.handleOnMouseEnterForth}
+          onMouseLeave={this.handleOnMouseLeaveForth}
+          className="section"
+        >
+          <img
+            src={
+              this.state.isHoveredForth ? forthSectionImgA : forthSectionImgB
+            }
+            alt="test"
+          />
+          <div className="textSetion">
+            <div className="letterHolder forthA"></div>
+            <div className="letterHolder forthB"></div>
+          </div>
+        </div>
       </div>
     );
   }
